eed81d9b9fc99619ad2c34f2c09a0eb416c8c696
VictorDemerji <demerji.v1@gmail.com>
2025-11-29 14:30:21 +0200

    $

---
+++ src/main/java/pixel/academy/crud_app/dao/StudentDAOImplementation.java
@@ -0,0 +1,10 @@
+package pixel.academy.crud_app.dao;
+
+import pixel.academy.crud_app.entity.Student;
+
+public class StudentDAOImplementation implements StudentDAO {
+    @Override
+    public void save(Student theStudent) {
+
+    }
+}
