d652910e2c88c68c78beabe8cdd04db50d3ab81f
VictorDemerji <demerji.v1@gmail.com>
2025-12-03 19:24:24 +0200

    chore:add multiple methods for creating students

--- src/main/java/pixel/academy/crud_app/CrudAppApplication.java
+++ src/main/java/pixel/academy/crud_app/CrudAppApplication.java
@@ -4,6 +4,8 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import pixel.academy.crud_app.dao.StudentDAO;
+import pixel.academy.crud_app.entity.Student;
 
 @SpringBootApplication
 public class CrudAppApplication {
@@ -13,11 +15,40 @@ public class CrudAppApplication {
 	}
 
     @Bean
-    public CommandLineRunner commandLineRunner(String[] args) {
+    public CommandLineRunner commandLineRunner(StudentDAO studentDAO) {
 
         return runner -> {
-            System.out.println("Hello, world!");
+            //createStudent(studentDAO);
+            createMultileStudents(studentDAO);
         };
+    }
+
+    private void createStudent(StudentDAO studentDao) {
+
+        //create a Student object
+        System.out.println("Creating new student object ...");
+        Student newStudent = new Student("John", "Doe", "john@pixelacademy.md");
+
+        //save Student object in data baze using DAO
+        System.out.println("Saving de student ...");
+        studentDao.save(newStudent);
+
+        // disply saved Student ID
+        System.out.println("Saved student. Generated id: " + newStudent.getId());
+    }
+
+    private void createMultileStudents(StudentDAO studentDAO) {
+        //create more students
+        System.out.println("Creating 3 student objects ...");
+        Student newStudent1 = new Student("Andrei", "Munteanu", "munteanu@pixelacedemy.md");
+        Student newStudent2 = new Student("Iulian","Vataman", "iulic@pixelacedemy.md");
+        Student newStudent3 = new Student("Maria", "Mirabela","mira@pixelacademy.md");
+
+        //save student objects in data baze
+        System.out.println("Saving the students ...");
+        studentDAO.save(newStudent1);
+        studentDAO.save(newStudent2);
+        studentDAO.save(newStudent3);
     }
 
 }
--- src/main/java/pixel/academy/crud_app/dao/StudentDAOImplementation.java
+++ src/main/java/pixel/academy/crud_app/dao/StudentDAOImplementation.java
@@ -1,10 +1,28 @@
 package pixel.academy.crud_app.dao;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityManager;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
 import pixel.academy.crud_app.entity.Student;
 
+@Repository
 public class StudentDAOImplementation implements StudentDAO {
-    @Override
-    public void save(Student theStudent) {
 
+    //Fiel for EntityManager (will be used for injection with data baze)
+    private EntityManager entityManager;
+
+    //Inject EntityManager by constructor (practice recomand for testability and modularity)
+    @Autowired
+    public StudentDAOImplementation(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
+
+    //Implementing the save method to save a Student object to the database
+    @Override
+    @Transactional
+    public void save(Student theStudent) {
+    entityManager.persist(theStudent);
     }
 }
